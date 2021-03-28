27c67491243147b3a3032c8f10212d88c16ffed4
hosain-mohamed <shs442000@gmail.com>
2021-03-28 14:08:37 +0200

    update files

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Flutter Doctor App',
       theme: ThemeData(
         fontFamily: "Nunito",
         primarySwatch: Colors.blue,
